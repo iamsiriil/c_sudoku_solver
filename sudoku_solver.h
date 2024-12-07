1a1167b0d8360a45e9466a9c05b76e62f6d11ae2
siriil <siriil@cn-siriil.lan>
2024-12-07 10:13:09 +0000

    checker.c : update

--- sudoku_solver.h
+++ sudoku_solver.h
@@ -18,4 +18,10 @@ void		ss_free_matrix(unsigned char **matrix, int size);
 int		ss_test_input(int argc, char **argv);
 void		ss_parse_input(int argc, char **argv, unsigned char **matrix);
 
+// Checker functions
+int		ss_check_row(unsigned char **matrix, int row, unsigned char nbr);
+int		ss_check_col(unsigned char **matrix, int col, unsigned char nbr);
+int		ss_check_sgrid(unsigned char **matrix, int row, int col, int nbr);
+int		ss_checker(unsigned char **matrix, int row, int col, unsigned char nbr);
+
 #endif 
