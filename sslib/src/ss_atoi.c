6e5d2c70ea06d49e90a1c03a65872845ca45e98e
siriil <siriil@cn-siriil.lan>
2024-12-09 16:36:27 +0000

    update

--- sslib/src/ss_atoi.c
+++ sslib/src/ss_atoi.c
@@ -8,17 +8,16 @@ int	ss_atoi(const char *str)
 	while (ss_isspace(*str))
 		str++;
 	sign = 1;
-	if (*str == '-' || *str == '+')
-	{
+	if (*str == '-' || *str == '+') {
 		if (*str == '-')
 			sign *= -1;
 		str++;
 	}
 	nbr = 0;
-	while (ss_isdigit(*str))
-	{
+	while (ss_isdigit(*str)) {
 		nbr = nbr * 10 + (*str - 48);
 		str++;
 	}
+
 	return (nbr * sign);
 }
