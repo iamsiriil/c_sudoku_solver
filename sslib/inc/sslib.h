972fa65f7448831eb70cea8e20baa42e73e34593
siriil <siriil@protonmail.ch>
2025-02-03 20:19:30 +0000

    delete: sslib

--- sslib/inc/sslib.h
+++
@@ -1,8 +0,0 @@
-#ifndef SSLIB_H
-# define SSLIB_H
-
-int	ss_atoi(const char *str);
-int	ss_isspace(int c);
-int	ss_isdigit(int c);
-
-#endif
--- sslib/src/ss_isdigit.c
+++
@@ -1,6 +0,0 @@
-#include "../inc/sslib.h"
-
-int	ss_isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
