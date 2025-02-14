287ad6564374035743b3d55145d7241edb90c04b
siriil <siriil@protonmail.ch>
2025-02-14 18:14:04 +0000

    update: error_handler.h

--- inc/error_handler.h
+++ inc/error_handler.h
@@ -1,9 +1,6 @@
 #ifndef ERROR_HANDLER_H
 # define ERROR_HANDLER_H
 
-# include <stdlib.h>
-# include <stdio.h>
-
 # define ERR_ALLOC "Failed to allocate memory"
 # define ERR_OFILE "Failed to open file"
 # define ERR_RFILE "Failed to read file"
